7c646d32ff80878f747195d1ce34e052da998751
Danil Antoshin <antoshindanil@ya.ru>
2020-12-04 21:01:00 +0200

    init

---
+++ config/application.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require_relative "boot"
+
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+# require "sprockets/railtie"
+require "rails/test_unit/railtie"
+
+Bundler.require(*Rails.groups)
+
+module MyTasksNg
+  class Application < Rails::Application
+    config.load_defaults 6.0
+    config.api_only = true
+    config.generators do |g|
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false
+    end
+  end
+end
